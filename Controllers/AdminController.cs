5d1a34da5425f57fd403f201f60b2fbf2ac043f8
T3rminator2297 <francis2297@hotmail.com>
2024-02-27 12:55:41 -0500

    Ajout de la page de gestion d'utilisateur et de permission

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Projet_Finale.Models;
+
+namespace Projet_Finale.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public AdminController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://127.0.0.1:5000");
+        }
+
+        public async Task<ActionResult> Permissions()
+        {
+            var response = await _httpClient.GetAsync("/all_users");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var userList = JsonConvert.DeserializeObject<UserList>(content);
+                return View(userList.Users);
+            }
+            else
+            {
+                return View("Error", "Une erreur s'est produite lors du chargement des utilisateurs. Veuillez réessayer plus tard.");
+            }
+        }
+    }
+}
