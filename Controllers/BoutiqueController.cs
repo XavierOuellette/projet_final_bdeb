6b114ae03e1998715deba51bd85e4095eab08daf
WilliamLebeuf <lebeufwilliam@gmail.com>
2024-03-04 08:54:30 -0500

    Préparation des views pour boutique, vision, faq

---
+++ Controllers/BoutiqueController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projet_Finale.Controllers
+{
+    public class BoutiqueController : Controller
+    {
+        public IActionResult Boutique()
+        {
+            return View();
+        }
+    }
+}
