f48ce99275f797c3efa8e56fd2ad620175f71c4a
T3rminator2297 <francis2297@hotmail.com>
2024-03-14 15:02:49 -0400

    +Ajout model +ajout vue insertitem

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projet_Finale.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Price { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public bool Available { get; set; }
+    }
+}
