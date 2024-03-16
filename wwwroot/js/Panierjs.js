32aa56bf2768a755f86a4e6536a4c8212cc7b0f2
WilliamLebeuf <lebeufwilliam@gmail.com>
2024-03-16 19:04:51 -0400

    Ajout Images placehoder + fix Jquery script

--- wwwroot/js/Panierjs.js
+++ wwwroot/js/Panierjs.js
@@ -1,21 +1,17 @@
-﻿$('.like-btn').on('click', function () {
-    $(this).toggleClass('is-active');
-});
-
-$('.minus-btn').on('click', function (e) {
+﻿$('.minus-btn').on('click', function (e) {
     e.preventDefault();
     var $this = $(this);
     var $input = $this.closest('div').find('input');
     var value = parseInt($input.val());
 
-    if (value & amp; amp; gt; 1) {
-    value = value - 1;
-} else {
-    value = 0;
-}
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
 
-$input.val(value);
- 
+    $input.val(value);
+
 });
 
 $('.plus-btn').on('click', function (e) {
@@ -24,11 +20,15 @@ $('.plus-btn').on('click', function (e) {
     var $input = $this.closest('div').find('input');
     var value = parseInt($input.val());
 
-    if (value & amp; amp; lt; 100) {
-    value = value + 1;
-} else {
-    value = 100;
-}
+    if (value < 100) {
+        value = value + 1;
+    } else {
+        value = 100;
+    }
 
-$input.val(value);
+    $input.val(value);
+});
+
+$('.like-btn').on('click', function () {
+    $(this).toggleClass('is-active');
 });
