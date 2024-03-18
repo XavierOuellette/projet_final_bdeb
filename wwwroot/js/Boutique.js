01638274304c4682802760e10c52831f39b4dcc5
XavierOuellette <137076419+XavierOuellette@users.noreply.github.com>
2024-03-17 20:45:19 -0400

    JS -> Razor View

--- wwwroot/js/Boutique.js
+++ wwwroot/js/Boutique.js
@@ -75,39 +75,12 @@ function convertCentsToDollars(priceInCents) {
 async function loadProducts() {
     // Fonction pour charger les produits depuis l'API et les afficher sur la page
     try {
-        const response = await fetch('http://127.0.0.1:5000/all_items');
-        const data = await response.json();
 
         const productsContainer = document.getElementById('productsContainer');
 
-        data.items.forEach(item => {
-            const productDiv = document.createElement('div');
-            productDiv.classList.add('col-lg-3', 'col-sm-6', 'col-md-3', 'productDiv');
-            productDiv.innerHTML = `
-                            <div class="box-img" item-id = "${item.id}">
-                                <h4>${item.name}</h4>
-                                <img src="${item.image_path}" alt="${item.name}" style="width:108px; height:112px;" />
-                                <div class="price-available">
-                                    <br>
-                                    <span class="price">${convertCentsToDollars(item.price)} $</span>
-                                    <br>
-                                    <span class="availability ${item.available == '1' ? 'available' : 'unavailable'}">${item.available == '1' ? 'Disponible' : 'Indisponible'}</span>
-                                    <div class="description" style="display: none;">
-                                        <br>
-                                        ${item.description}
-                                    </div>
-                                    <br>
-                                    <button class="edit-button" style="display: none;">
-                                        <i class="bi bi-pencil"></i>
-                                    </button>
-                                    <button class="delete-button" style="display: none;">
-                                        <i class="bi bi-trash"></i>
-                                    </button>
-                                </div>
-                            </div>
-                    `;
+        Array.from(productsContainer.children).forEach(product => {
             // Ajouter un gestionnaire d'événements pour agrandir le productDiv et afficher la description
-            productDiv.onclick = function (event) {
+            product.onclick = function (event) {
                 const descriptionDiv = this.querySelector('.description');
                 const editButton = this.querySelector('.edit-button');
                 const deleteButton = this.querySelector('.delete-button');
@@ -124,7 +97,7 @@ async function loadProducts() {
             }
 
             // Ajouter un gestionnaire d'événements pour le bouton delete
-            const deleteButton = productDiv.querySelector('.delete-button');
+            const deleteButton = product.querySelector('.delete-button');
              deleteButton.addEventListener('click', function (event) {
                 event.stopPropagation(); // Arrêter la propagation de l'événement de clic
 
@@ -168,8 +141,6 @@ async function loadProducts() {
                     });
                 }
             });
-
-            productsContainer.appendChild(productDiv);
         });
     } catch (error) {
         console.error('Erreur lors du chargement des produits :', error);
